0bcd6bcc992d5ed2942c7a24332773cf3d295b2f
Eric Jadi <erjadi@microsoft.com>
2023-08-01 00:07:08 +0200

    fix for SDXL

--- training_scripts/run_lora_db_unet_only.sh
+++ training_scripts/run_lora_db_unet_only.sh
@@ -1,17 +1,18 @@
 #https://github.com/huggingface/diffusers/tree/main/examples/dreambooth
 export MODEL_NAME="stabilityai/stable-diffusion-2-1-base"
-export INSTANCE_DIR="./data_example"
-export OUTPUT_DIR="./output_example"
+export INSTANCE_DIR="../../cog_instance_data"
+export OUTPUT_DIR="../../cog_output"
 
 accelerate launch train_dreambooth_lora_sdxl.py \
-  --pretrained_model_name_or_path=$MODEL_NAME  \
+  --pretrained_model_name_or_path=$5  \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
-  --instance_prompt="style of sks" \
-  --resolution=512 \
+  --instance_prompt=$1 \
+  --resolution=$3 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
-  --learning_rate=1e-4 \
+  --learning_rate=$4 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --max_train_steps=30000
+  --max_train_steps=$2 \
+  --mixed_precision=fp16
